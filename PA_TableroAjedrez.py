2135f1aae9e03b4780684890d177819c2ceb0601
Rommel Guaman <rom2elguaman@gmail.com>
2016-06-01 22:27:15 -0500

    TableroAjedrez1.3
    Listo

--- PA_TableroAjedrez.py
+++ PA_TableroAjedrez.py
@@ -1,6 +1,9 @@
+#Importamos las librerias necesarias
 import turtle
-import os
 
+
+
+#asignamos las caracteristicas la ventana que mostrara el tablero
 wind = turtle.Screen()
 wind.bgcolor('lightblue')
 wind.title('Tablero ajedrez')
@@ -8,6 +11,7 @@ wind.title('Tablero ajedrez')
 tab=turtle.Turtle()
 tab.speed(0)
 
+#dibujamos el cuadro
 def cuadrado(tam,var,color):
 	tab.color(color)
 	tab.begin_fill()
@@ -17,6 +21,7 @@ def cuadrado(tam,var,color):
 	tab.end_fill()
 	tab.fd(tam)
 
+#dibujamos las filas
 def fila(tam,var,color1,color2):
 	for i in range(4):
 		if(var==True):
@@ -26,6 +31,20 @@ def fila(tam,var,color1,color2):
 			cuadrado(tam,var,color2)
 			cuadrado(tam,var,color1)
 
+#dibujamos tablero
 def tablero(tam,var,color1,color2):
 	tab.pu()
 	tab.goto(-(tam*4),(tam*4))
+	for i in range(8):
+		fila(tam,var,color1,color2)
+		tab.bk(tam*8)
+		tab.rt(90)
+		tab.fd(tam)
+		tab.lt(90)
+		if(var==True):
+			var=False
+		else:
+			var=True
+	turtle.mainloop()
+
+tablero(50,True,'black','white')
