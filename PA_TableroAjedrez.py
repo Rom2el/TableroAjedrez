1857c9a6290281ef84efee58a0f983b66c42e57d
KarlaSteff <karliz_steffania@hotmail.com>
2016-06-01 21:34:56 -0500

    Listas y Filas
    Añadir el último bucle. :D

--- PA_TableroAjedrez.py
+++ PA_TableroAjedrez.py
@@ -16,3 +16,16 @@ def cuadrado(tam,var,color):
 		tab.lt(90)
 	tab.end_fill()
 	tab.fd(tam)
+
+def fila(tam,var,color1,color2):
+	for i in range(4):
+		if(var==True):
+			cuadrado(tam,var,color1)
+			cuadrado(tam,var,color2)
+		else:
+			cuadrado(tam,var,color2)
+			cuadrado(tam,var,color1)
+
+def tablero(tam,var,color1,color2):
+	tab.pu()
+	tab.goto(-(tam*4),(tam*4))
